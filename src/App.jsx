5cf99c9e20b9b5301acd3099a85e7956a1f89e96
alisaker <alisaker-dev>
2026-07-12 20:56:13 +0300

    import style.css in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
+import "./style.css";
 
 export default function App() {
   return (
